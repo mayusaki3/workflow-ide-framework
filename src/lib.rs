2c7cae3efe44f28f698f1d46091580dfe2216073
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2026-09-19 05:06:35 +0900

    Step 1: adapt framework host to eframe 0.34 App API

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,24 @@ pub struct Application {
 
 impl Application {
     pub fn new(id: impl Into<String>, name: impl Into<String>) -> Self {
-        Self { id: id.into(), name: name.into() }
+        Self {
+            id: id.into(),
+            name: name.into(),
+        }
     }
 
     pub fn run(self) -> eframe::Result<()> {
-        let title = self.name.clone();
+        let window_title = self.name.clone();
+        let app_name = self.name;
         let app_id = self.id;
+
         eframe::run_native(
-            &title,
+            &window_title,
             eframe::NativeOptions::default(),
             Box::new(move |_cc| {
                 Ok(Box::new(FrameworkHost {
                     app_id,
-                    app_name: title,
+                    app_name,
                 }))
             }),
         )
@@ -32,11 +37,9 @@ struct FrameworkHost {
 }
 
 impl eframe::App for FrameworkHost {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(&self.app_name);
-            ui.label(format!("Application ID: {}", self.app_id));
-            ui.label("workflow-ide-framework v0.1.0 Step 1 probe");
-        });
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        ui.heading(&self.app_name);
+        ui.label(format!("Application ID: {}", self.app_id));
+        ui.label("workflow-ide-framework v0.1.0 Step 1 sample");
     }
 }
