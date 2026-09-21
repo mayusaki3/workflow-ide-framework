8398099107580d5213612881c75587bae5f4a75b
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2026-09-21 20:54:07 +0900

    Step 5.6: add Log Viewer sample

---
+++ examples/step5_6_log_viewer.rs
@@ -0,0 +1,9 @@
+use workflow_ide_framework::{Application, LayoutConfig, PanelDefinition, PanelKind};
+
+fn main() -> eframe::Result<()> {
+    Application::new("step5-6-log-viewer", "Step 5.6 Log Viewer")
+        .panel(PanelDefinition::new("logs", "Logs", PanelKind::StandardUi))
+        .layout(LayoutConfig::new(vec!["logs".into()]))
+        .log_viewer_panel("logs")
+        .run()
+}
