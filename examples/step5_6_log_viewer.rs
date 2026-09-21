e9d4029ee75b9bf4c09feca47a4e7b5455e8c50a
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2026-09-21 21:04:08 +0900

    Step 5.6: fix Log Viewer sample layout type inference

--- examples/step5_6_log_viewer.rs
+++ examples/step5_6_log_viewer.rs
@@ -3,7 +3,7 @@ use workflow_ide_framework::{Application, LayoutConfig, PanelDefinition, PanelKi
 fn main() -> eframe::Result<()> {
     Application::new("step5-6-log-viewer", "Step 5.6 Log Viewer")
         .panel(PanelDefinition::new("logs", "Logs", PanelKind::StandardUi))
-        .layout(LayoutConfig::new(vec!["logs".into()]))
+        .layout(LayoutConfig::new(["logs"]))
         .log_viewer_panel("logs")
         .run()
 }
